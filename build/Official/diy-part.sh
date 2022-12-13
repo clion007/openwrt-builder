f306b38d0847d7be730fe8982841263e22248c18
281677160 <66651997+281677160@users.noreply.github.com>
2022-12-13 18:04:06 +0800

    Update diy-part.sh

--- build/Official/diy-part.sh
+++ build/Official/diy-part.sh
@@ -36,7 +36,6 @@ export Personal_Signature="$(TZ=UTC-8 date "+%Y.%m.%d")"  # 个性签名,你想�
 
 # 修改插件名字
 sed -i 's?"终端"?"TTYD"?g' 2>/dev/null `grep -Eorl '"终端"' 'feeds' 'package'`
-sed -i 's?"aMule设置"?"电驴下载"?g' 2>/dev/null `grep -Eorl '"aMule设置"' 'feeds' 'package'`
 sed -i 's?"网络存储"?"NAS"?g' 2>/dev/null `grep -Eorl '"网络存储"' 'feeds' 'package'`
 sed -i 's?"实时流量监测"?"流量"?g' 2>/dev/null `grep -Eorl '"实时流量监测"' 'feeds' 'package'`
 sed -i 's?"KMS 服务器"?"KMS激活"?g' 2>/dev/null `grep -Eorl '"KMS 服务器"' 'feeds' 'package'`
