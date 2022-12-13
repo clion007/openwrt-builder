610d19e5993e53469110ec1a8057938fe87499d1
281677160 <66651997+281677160@users.noreply.github.com>
2022-12-13 18:22:36 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -35,16 +35,16 @@ export Personal_Signature="$(TZ=UTC-8 date "+%Y.%m.%d")"  # 个性签名,你想�
 
 
 # 修改插件名字
-sed -i 's?"aMule设置"?"电驴下载"?g' 2>/dev/null `grep -Eorl '"aMule设置"' 'feeds' 'package'`
-sed -i 's?"网络存储"?"NAS"?g' 2>/dev/null `grep -Eorl '"网络存储"' 'feeds' 'package'`
-sed -i 's?"Turbo ACC 网络加速"?"网络加速"?g' 2>/dev/null `grep -Eorl '"Turbo ACC 网络加速"' 'feeds' 'package'`
-sed -i 's?"实时流量监测"?"流量"?g' 2>/dev/null `grep -Eorl '"实时流量监测"' 'feeds' 'package'`
-sed -i 's?"KMS 服务器"?"KMS激活"?g' 2>/dev/null `grep -Eorl '"KMS 服务器"' 'feeds' 'package'`
-sed -i 's?"TTYD 终端"?"TTYD"?g' 2>/dev/null `grep -Eorl '"TTYD 终端"' 'feeds' 'package'`
-sed -i 's?"USB 打印服务器"?"打印服务"?g' 2>/dev/null `grep -Eorl '"USB 打印服务器"' 'feeds' 'package'`
-sed -i 's?"Web 管理"?"Web管理"?g' 2>/dev/null `grep -Eorl '"Web 管理"' 'feeds' 'package'`
-sed -i 's?"管理权"?"改密码"?g' 2>/dev/null `grep -Eorl '"管理权"' 'feeds' 'package'`
-sed -i 's?"带宽监控"?"监控"?g' 2>/dev/null `grep -Eorl '"带宽监控"' 'feeds' 'package'`
+sed -i 's?"aMule设置"?"电驴下载"?g' 2>/dev/null `grep -Eo '"aMule设置"' -rl 'feeds' 'package'`
+sed -i 's?"网络存储"?"NAS"?g' 2>/dev/null `grep -Eo '"网络存储"' -rl 'feeds' 'package'`
+sed -i 's?"Turbo ACC 网络加速"?"网络加速"?g' 2>/dev/null `grep -Eorl '"Turbo ACC 网络加速"' -rl 'feeds' 'package'`
+sed -i 's?"实时流量监测"?"流量"?g' 2>/dev/null `grep -Eo '"实时流量监测"' -rl 'feeds' 'package'`
+sed -i 's?"KMS 服务器"?"KMS激活"?g' 2>/dev/null `grep -Eo '"KMS 服务器"' -rl 'feeds' 'package'`
+sed -i 's?"TTYD 终端"?"TTYD"?g' 2>/dev/null `grep -Eo '"TTYD 终端"' -rl 'feeds' 'package'`
+sed -i 's?"USB 打印服务器"?"打印服务"?g' 2>/dev/null `grep -Eo '"USB 打印服务器"' -rl 'feeds' 'package'`
+sed -i 's?"Web 管理"?"Web管理"?g' 2>/dev/null `grep -Eo '"Web 管理"' -rl 'feeds' 'package'`
+sed -i 's?"管理权"?"改密码"?g' 2>/dev/null `grep -Eo '"管理权"' -rl 'feeds' 'package'`
+sed -i 's?"带宽监控"?"监控"?g' 2>/dev/null `grep -Eo '"带宽监控"' -rl 'feeds' 'package'`
 
 
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
