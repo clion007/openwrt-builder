f495c8051d43966e443e5754a79486d98ccf028b
281677160 <66651997+281677160@users.noreply.github.com>
2024-03-12 19:14:00 +0800

    Update diy-part1.sh

--- build/Lede/diy-part1.sh
+++ build/Lede/diy-part1.sh
@@ -11,3 +11,6 @@ export PassWall_luci_branch="0"             # passwall的源码分别有【luci�
 # 替换OpenClash的源码(默认master分支)
 export OpenClash_branch="0"                 # OpenClash的源码分别有【master分支】和【dev分支】(填0为使用master分支,填1为使用dev分支)
 export OpenClash_Core="1"                   # 增加OpenClash时,把核心下载好,(填1为下载【dev单核】,填2为下载【dev/meta/premium三核】,填0为不需要核心)
+
+# 增加AdGuardHome插件和核心
+export AdGuardHome_Core="0"                  # 编译固件时自动增加AdGuardHome插件和AdGuardHome插件核心,需要注意的是一个核心20多MB的,小闪存机子搞不来(1为启用命令,填0为不作修改)
