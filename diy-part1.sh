2a8c0b2e887a643104cdada8022ebfb6ed100513
clion007 <clion007@126.com>
2024-04-27 16:10:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,5 +12,5 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git coolsnowwolf https://github.com/coolsnowwolf/lede' >>feeds.conf.default
+echo 'src-git lede https://github.com/coolsnowwolf/lede' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
