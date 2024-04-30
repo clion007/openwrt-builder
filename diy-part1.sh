0befb692a8a0eb21b46c0f47aad920baf80a6834
clion007 <clion007@126.com>
2024-04-30 19:38:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,4 +12,4 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+#echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
