215a200c99cb5c7f4d66edc04c7fef86ee2fc8a0
clion007 <clion007@126.com>
2024-04-30 10:25:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git lienolpackages https://github.com/Lienol/openwrt-packages.git;23.05' >>feeds.conf.default
-echo 'src-git lienolluci https://github.com/Lienol/openwrt-luci.git;23.05' >>feeds.conf.default
-echo 'src-git leanpackages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
-echo 'src-git leanluci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src/gz openwrt_kiddin9 https://dl.openwrt.ai/latest/packages/x86_64/kiddin9' >>feeds.conf.default
