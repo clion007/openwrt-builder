49105a8718e208b8c8bda1ed4571c216c9f565cc
clion007 <clion007@126.com>
2024-04-30 09:34:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,3 +14,5 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git lienolpackages https://github.com/Lienol/openwrt-packages.git;23.05' >>feeds.conf.default
 echo 'src-git lienolluci https://github.com/Lienol/openwrt-luci.git;23.05' >>feeds.conf.default
+echo 'src-git leanpackages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+echo 'src-git leanluci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
