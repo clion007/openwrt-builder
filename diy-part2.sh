5c403f35190306a83d7f8a5560cf92991a7a261a
clion007 <clion007@126.com>
2024-05-05 14:02:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,14 @@ sed -i 's/192.168.1.1/192.168.101.1/g' package/base-files/files/bin/config_gener
 
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+if [ -d openwrt/package/lean ]; then 
+  cd openwrt/package/lean
+  rm -rf luci-app-argon-config
+  git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config
+else
+  cd openwrt/package
+  git clone https://github.com/jerrykuku/luci-app-argon-config.git
+fi
 
 # Modify hostname and timezone
 sed -i 's/OpenWrt/Clion-Router/g' package/base-files/files/bin/config_generate
