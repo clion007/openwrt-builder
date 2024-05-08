50e6c3ca5a764956d4433b9a522ac2c7d8697022
clion007 <clion007@126.com>
2024-05-08 22:30:42 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,6 +9,4 @@
 #
 
 # Add other feed sources
-# echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 # echo "src-git istore https://github.com/linkease/istore;main" >> "feeds.conf.default"
