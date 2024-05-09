1b2626a5eca98cc1fe0d299a4766883a2336d878
clion007 <clion007@126.com>
2024-05-09 10:24:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -33,8 +33,10 @@ rm -rf $bgdir/*
 cp -r $GITHUB_WORKSPACE/source/img/* $bgdir/
 
 # Add passwall application
-git clone -b main https://github.com/xiaorouji/openwrt-passwall.git ./feeds/luci/passwall
-git clone -b main https://github.com/xiaorouji/openwrt-passwall-packages.git ./feeds/passwall-packages
+git clone -b main https://github.com/xiaorouji/openwrt-passwall.git ./feeds/luci/applications/passwall
+mv -rf ./feeds/luci/applications/passwall/* ./feeds/luci/applications/
+rm -rf ./feeds/luci/applications/passwall
+git clone -b main https://github.com/xiaorouji/openwrt-passwall-packages.git ./feeds/packages/passwall
 
 # Add istore application
 git clone -b main https://github.com/linkease/istore.git ./feeds/luci/applications/istore
